bfa435d973b6810bbe3c646eab3a24cb6d020cc7
Gordon Gustafson <gordon3.14@gmail.com>
2025-05-23 11:37:03 -0700

    Don't use py_limited_api in setup.py

--- torch_extensions/causal_multihead_self_attention/setup.py
+++ torch_extensions/causal_multihead_self_attention/setup.py
@@ -4,12 +4,8 @@ from torch.utils import cpp_extension
 setup(name="causal_multihead_self_attention",
       ext_modules=[
           cpp_extension.CUDAExtension(
-            "causal_multihead_self_attention",
-            ["causal_multihead_self_attention.cu"],
-            # define Py_LIMITED_API with min version 3.9 to expose only the stable
-            # limited API subset from Python.h
-            extra_compile_args={"cxx": ["-DPy_LIMITED_API=0x03090000"]},
-            py_limited_api=True)],  # Build 1 wheel across multiple Python versions
+              "causal_multihead_self_attention",
+              ["causal_multihead_self_attention.cu"],
+              py_limited_api=False)],  # I couldn't get py_limited_api=True to work.
       cmdclass={'build_ext': cpp_extension.BuildExtension},
-      options={"bdist_wheel": {"py_limited_api": "cp39"}}  # 3.9 is minimum supported Python version
 )
