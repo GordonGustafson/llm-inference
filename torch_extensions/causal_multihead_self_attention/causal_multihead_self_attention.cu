100f421dd18e91ce3c9fd5a92e5816a97381f4f1
Gordon Gustafson <gordon3.14@gmail.com>
2025-05-28 16:22:18 -0700

    Try avoiding shm bank conflicts when writing Q

--- torch_extensions/causal_multihead_self_attention/causal_multihead_self_attention.cu
+++ torch_extensions/causal_multihead_self_attention/causal_multihead_self_attention.cu
@@ -66,11 +66,12 @@ __global__ void __launch_bounds__(1024)
     int const B_r_bounds_checked_for_last_row = min(B_r, N - blockIdx.x * B_r);
     int const d_min_for_head = blockIdx.y * d_head;
 
-    // Avoid bank conflicts when writing K to shared memory.
+    // Avoid bank conflicts when writing Q and K to shared memory.
+    int const Q_row_length = B_r + 1;
     int const K_row_length = B_c + 1;
 
     float* const Q = sharedMemory;
-    float* const K = Q + B_r * d_head;
+    float* const K = Q + Q_row_length * d_head;
     float* const V = K + K_row_length * d_head;
     float* const S = V + B_c * d_head;
 
@@ -80,7 +81,7 @@ __global__ void __launch_bounds__(1024)
         for (int B_r_index = threadIdx.y; B_r_index < B_r_bounds_checked_for_last_row; B_r_index += blockDim.y) {
             int const row_index = blockIdx.x * B_r + B_r_index;
             // Store Q in shm in transposed form to optimize for coalesced reads.
-            Q[d_index * B_r + B_r_index] = Q_HBM[row_index * d_model + d_min_for_head + d_index];
+            Q[d_index * Q_row_length + B_r_index] = Q_HBM[row_index * d_model + d_min_for_head + d_index];
         }
     }
 
@@ -113,7 +114,7 @@ __global__ void __launch_bounds__(1024)
                 float S_val_for_thread = 0.0f;
                 #pragma unroll
                 for (int d_index = 0; d_index < d_head; d_index++) {
-                    S_val_for_thread += Q[d_index * B_r + B_r_index] * K[d_index * K_row_length + threadIdx.x];
+                    S_val_for_thread += Q[d_index * Q_row_length + B_r_index] * K[d_index * K_row_length + threadIdx.x];
                 }
                 S[B_r_index * B_c + threadIdx.x] = S_val_for_thread / temperature;
             }
@@ -203,7 +204,7 @@ void causal_multihead_self_attention(float const* const Q,  // size Nxd
 
     dim3 const blocksPerGrid(T_r, num_heads);
     dim3 const threadsPerBlock(B_c, B_r);
-    int const sharedMemoryBytes = (B_r * d_head          // Q
+    int const sharedMemoryBytes = ((B_r + 1) * d_head    // Q
                                    + (B_c + 1) * d_head  // K
                                    + B_c * d_head        // V
                                    + B_r * B_c)          // S
