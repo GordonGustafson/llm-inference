c5a5c4b7a5574f4e18137fa6b6aa948bf6c03ec6
Gordon Gustafson <gordon3.14@gmail.com>
2025-05-28 14:08:58 -0700

    Pass -lineinfo and --use_fast_math flags to nvcc

--- torch_extensions/causal_multihead_self_attention/setup.py
+++ torch_extensions/causal_multihead_self_attention/setup.py
@@ -6,6 +6,7 @@ setup(name="causal_multihead_self_attention",
           cpp_extension.CUDAExtension(
               "causal_multihead_self_attention",
               ["causal_multihead_self_attention.cu"],
+              extra_compile_args={'nvcc': ['-lineinfo', '--use_fast_math']},
               py_limited_api=False)],  # I couldn't get py_limited_api=True to work.
       cmdclass={'build_ext': cpp_extension.BuildExtension},
 )
